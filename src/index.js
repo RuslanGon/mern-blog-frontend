1a6bde23708fc22ff059c14344a44ef4c2d179de
RuslanGon <ruslan441006@gmail.com>
2025-03-10 17:21:26 +0200

    wer

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -15,7 +17,9 @@ root.render(
     <CssBaseline />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-      <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
