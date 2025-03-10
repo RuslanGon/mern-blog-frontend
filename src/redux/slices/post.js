b9cdf0fe0828f73fae366fd8248c482951d1a690
RuslanGon <ruslan441006@gmail.com>
2025-03-10 18:48:29 +0200

    wer

--- src/redux/slices/post.js
+++ src/redux/slices/post.js
@@ -1,28 +1,40 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from '../../axios.js'
+import axios from '../../axios.js';
 
 export const fetchPosts = createAsyncThunk('/posts/fetchPosts', async () => {
-const {data} = await axios.get('/posts')
-return data
-})
+  const { data } = await axios.get('/posts');
+  return data;
+});
 
 const initialState = {
   posts: {
     items: [],
-    status: 'loading'
+    status: 'loading',
   },
   tags: {
     items: [],
-    status: 'loading'
+    status: 'loading',
   },
 };
 
 const postSlice = createSlice({
   name: "posts",
   initialState,
-  reducers: {
-   
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPosts.pending, (state) => {
+        state.posts.status = 'loading'; 
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.posts.items = action.payload; 
+        state.posts.status = 'succeeded'; 
+      })
+      .addCase(fetchPosts.rejected, (state) => {
+        state.posts.items = []
+        state.posts.status = 'error'; 
+      });
   },
 });
 
-export const postReducer =  postSlice.reducer;
+export const postReducer = postSlice.reducer;
