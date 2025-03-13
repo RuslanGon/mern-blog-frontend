d0f610c4a388276aa2650e4eebd680a4fbf2e681
RuslanGon <ruslan441006@gmail.com>
2025-03-13 15:33:11 +0200

    wer

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -7,30 +7,53 @@ import { useForm } from "react-hook-form";
 import styles from "./Login.module.scss";
 
 export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setError,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    // mode: 'onChange'
+  });
 
-const {register, handleSubmit, setError} = useForm({
-  defaultValues: {
-    email: '',
-    password: ''
-  }
-})
+  const onSubmit = (values) => {
+    console.log(values);
+    reset()
+  };
 
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Вход в аккаунт
       </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Неверно указана почта"
-        fullWidth
-      />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Войти
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          type="email"
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          {...register("email", { required: "Укажите почту" })}
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          label="Пароль"
+          type="password"
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          {...register("password", { required: "Укажите пароль" })}
+          fullWidth
+        />
+        <Button type="submit" size="large" variant="contained" fullWidth>
+          Войти
+        </Button>
+      </form>
     </Paper>
   );
 };
